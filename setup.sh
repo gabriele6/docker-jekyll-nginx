16b0d9d0f9047fcba26cfa260d72574314c97d57
gabriele6 <gabriele6@users.noreply.github.com>
2016-04-22 22:00:20 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,30 +3,30 @@ FILE="sites.txt"
 mkdir /usr/share/nginx/sites
 
 while read STRING; do
-	touch /etc/nginx/sites-available/$STRING
-	echo "server {
+        touch /etc/nginx/sites-available/$STRING
+        echo "server {
         listen 80;
         root /usr/share/nginx/sites/$STRING;
         index index.php index.html index.htm;
         server_name $STRING;
       }" >> /etc/nginx/sites-available/$STRING
-	mkdir /usr/share/nginx/sites/$STRING
-	ln -s /etc/nginx/sites-available/$STRING /etc/nginx/sites-enabled/$STRING
+        mkdir /usr/share/nginx/sites/$STRING
+        ln -s /etc/nginx/sites-available/$STRING /etc/nginx/sites-enabled/$STRING
 
-	cd
-	NM=$STRING
-	read STRING
-	wget $STRING -O archive.zip
-	unzip archive.zip
-	
-	NEW_DIR=$(ls -Art | tail -n 1)
-	cd $NEW_DIR
-	
-	CONT=$(docker run -d -v "/$PWD/$NEW_DIRECTORY:/src" grahamc/jekyll build)
-	docker rm $CONT
-	rm -r /usr/share/nginx/sites/$NM/*
-	cp -r _site/* /usr/share/nginx/sites/$NM
-	cd
-	rm archive.zip
-	rm -r $NEW_DIR
+        cd
+        NM=$STRING
+        read STRING
+        wget $STRING -O archive.zip
+        unzip archive.zip
+        
+        NEW_DIR=$(ls -dt */ | head -1| cut -d'/' -f1)
+        cd $NEW_DIR
+        echo SONO DENTRO $NEW_DIR
+        CONT=$(docker run --rm -v "/$PWD:/src" grahamc/jekyll build)
+        docker rm $CONT
+        rm -r /usr/share/nginx/sites/$NM/*
+        cp -r _site/* /usr/share/nginx/sites/$NM
+        cd
+        rm archive.zip
+        rm -r $NEW_DIR
 done < "$FILE"
