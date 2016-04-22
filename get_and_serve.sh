0d7566f8246108b8bc617de479ab41d6d1447334
gabriele6 <gabriele6@users.noreply.github.com>
2016-04-22 12:11:54 +0200

    Update get_and_serve.sh

--- get_and_serve.sh
+++ get_and_serve.sh
@@ -6,15 +6,6 @@ REPO="spectral" # the repository name of the theme you want to use
 BRANCH="master"
 NEW_DIRECTORY="personal"
 
-# halting and deleting the obsolete containers
-# NOTE: do not edit "config.txt". Otherwise the script might not halt the old containers and the updated files won't be built
-while read STRING; do
-	docker stop $STRING
-	docker rm $STRING
-done < "$FILE"
-
-rm ./$FILE
-
 cd
 
 # deleting old content in the obsolete directory
