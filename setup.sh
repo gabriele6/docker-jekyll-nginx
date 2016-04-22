2e6721b8f4e56becb5621d8ea373ec80137e2289
gabriele6 <gabriele6@users.noreply.github.com>
2016-04-22 21:02:57 +0200

    Added files via upload

--- setup.sh
+++ setup.sh
@@ -1,18 +1,31 @@
-BRANCH="master"
-wget "https://github.com/gabriele6/docker-jekyll-nginx/archive/$BRANCH.zip"
-unzip "$BRANCH.zip"
-yes | cp -i -r docker-jekyll-nginx-$BRANCH/* ./
-rm -r docker-jekyll-nginx-$BRANCH/ $BRANCH.zip README.md
+FILE="sites.txt"
 
-sudo chmod 777 build_and_serve.sh
-sudo chmod 777 get_and_serve.sh
-sudo chmod 777 restart_containers.sh
-sudo chmod 777 removeAll.sh
-sudo chmod 777 closeAll.sh
-sudo chmod 777 setup.sh
+mkdir /usr/share/nginx/sites
 
+while read STRING; do
+	touch /etc/nginx/sites-available/$STRING
+	echo "server {
+        listen 80;
+        root /usr/share/nginx/sites/$STRING;
+        index index.php index.html index.htm;
+        server_name $STRING;
+      }" >> /etc/nginx/sites-available/$STRING
+	mkdir /usr/share/nginx/sites/$STRING
+	ln -s /etc/nginx/sites-available/$STRING /etc/nginx/sites-enabled/$STRING
 
-./closeAll.sh
-./removeAll.sh
-
-./get_and_serve.sh
+	cd
+	NM=$STRING
+	read STRING
+	wget $STRING -O archive.zip
+	unzip archive.zip
+	
+	NEW_DIR=$(ls -Art | tail -n 1)
+	rm archive.zip
+	cd $NEW_DIR
+	
+	CONT=$(docker run -d -v "/$PWD/$NEW_DIRECTORY:/src" grahamc/jekyll build)
+	docker rm $CONT
+	cp -r _site/* /usr/share/nginx/sites/$NM
+	cd 
+	rm -r $NEW_DIR
+done < "$FILE"
