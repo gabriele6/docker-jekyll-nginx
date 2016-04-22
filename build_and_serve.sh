437cd75f6c96d2dca1a7f5597356cdd0614c1bf4
gabriele6 <gabriele6@users.noreply.github.com>
2016-04-22 12:15:18 +0200

    Update build_and_serve.sh

--- build_and_serve.sh
+++ build_and_serve.sh
@@ -4,7 +4,7 @@ FILE="containers.txt"
 NEW_DIRECTORY="personal"
 
 # halting and deleting the obsolete containers
-# NOTE: do not edit "config.txt". Otherwise the script might not halt the old containers and the updated files won't be built
+# NOTE: do not edit "containers.txt". Otherwise the script might not halt the old containers and the updated files won't be built
 while read STRING; do
 	docker stop $STRING
 	docker rm $STRING
