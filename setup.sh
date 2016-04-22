c65eddf1210a99d0592c22c09b736dde78b28964
gabriele6 <gabriele6@users.noreply.github.com>
2016-04-22 11:37:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 BRANCH="master"
 wget "https://github.com/gabriele6/docker-jekyll-nginx/archive/$BRANCH.zip"
-unzip $BRANCH.zip
+unzip "$BRANCH.zip"
 yes | cp -i -r docker-jekyll-nginx-BRANCH/* ./
 rm -r docker-jekyll-nginx-master/ BRANCH.zip README.md
 
